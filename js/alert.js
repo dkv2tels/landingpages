cd406a5c934fdfce2311872e0640ce17b1f820ac
Developer DKV 2 <126973802+dkv2tels@users.noreply.github.com>
2023-03-10 05:15:56 +0700

    Create alert.js

---
+++ js/alert.js
@@ -0,0 +1,4 @@
+alert('Hai Kak, Ini Adalah Website X DKV 2')
+alert('Ini Adalah Update 1.0.0')
+alert('Yang Di Update Adalah, Landing Pages, Navbar, Dan Beberapa Components Kecil Lainnya')
+alert('Terima Kasih Telah Mengunjungi Website Ini Ya Kak ^_^')
