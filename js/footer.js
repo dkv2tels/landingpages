cd9d850a315402c06b47ce4466d66bac514acab8
Developer DKV 2 <126973802+dkv2tels@users.noreply.github.com>
2023-03-10 04:46:33 +0700

    X DKV 2

---
+++ js/footer.js
@@ -0,0 +1,3 @@
+let lokasi = "Tambun Selatan, Bekasi, Jawa Barat, Indonesia"
+let admin_number = "(+62) 813-3456-7203"
+let email_offc = "dkv2tels@gmail.com"
---
+++ js/profile-card.js
@@ -0,0 +1,21 @@
+let Email1 = "Coming Soon!"
+let Email2 = "owner@kenichidev.xyz"
+let Email3 = "Coming Soon!"
+
+let WA1 = "+62 812-8260-2305"
+let WA2 = "+62 813-3456-7203"
+let WA3 = "+62 897-9778-015"
+
+let IG1 = "@_ryuuzr"
+let IG2 = "@ichi_san_sad"
+let IG3 = "@rashif_.arsyah"
+
+let PT1 = "Coming Soon!"
+let PT2 = "Coming Soon!"
+let PT3 = "Coming Soon!"
+
+let NM1 = "Nikolas Saputra"
+let NM2 = "Kenichi Ichi ( RP )"
+let NM3 = "Rashif Arsyah"
+
+let Message = ""
